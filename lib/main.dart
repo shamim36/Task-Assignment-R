064c50fc11938b9fe89491c9f5313aa9ecdfffce
shamim36 <md.shamim.ahamed.888@gmail.com>
2023-09-18 15:46:38 +0600

    module 06 updated

--- lib/main.dart
+++ lib/main.dart
@@ -19,33 +19,63 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+  List<String>image=new List.empty(growable: true);
 
   @override
   Widget build(BuildContext context) {
+    image.add("https://th.bing.com/th/id/OIP.qA9zQh6fPwYYu4UQXn2AeAAAAA?pid=ImgDet&rs=1");
+    image.add("https://th.bing.com/th/id/OIP.qA9zQh6fPwYYu4UQXn2AeAAAAA?pid=ImgDet&rs=1");
+    image.add("https://th.bing.com/th/id/OIP.qA9zQh6fPwYYu4UQXn2AeAAAAA?pid=ImgDet&rs=1");
+    image.add("https://th.bing.com/th/id/OIP.qA9zQh6fPwYYu4UQXn2AeAAAAA?pid=ImgDet&rs=1");
+    image.add("https://th.bing.com/th/id/OIP.qA9zQh6fPwYYu4UQXn2AeAAAAA?pid=ImgDet&rs=1");
+    image.add("https://th.bing.com/th/id/OIP.qA9zQh6fPwYYu4UQXn2AeAAAAA?pid=ImgDet&rs=1");
+
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Photo Gallery')),
+        title: const Center(child: Text('Photo Gallery')),
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
-            Text('Welcome to My Photo Gallery!', style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),),
-            SizedBox(
+            const Text('Welcome to My Photo Gallery!', style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),),
+            const SizedBox(
               height: 10,
             ),
-            TextField(
+            const TextField(
               decoration: InputDecoration(
                 hintText: 'Search for photos...',
                 border: OutlineInputBorder(gapPadding: 10),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
+            ),
+
+            Container(
+
+              height: 750,
+              width: 750,
+              child:  GridView.builder(
+
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: image.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, crossAxisSpacing: 8, mainAxisSpacing: 4),
+                itemBuilder: (context,index){
+                  return  Card(
+                    elevation: 5,
+                    // child: Image.network(image[index],fit: BoxFit.fitWidth),
+                    child: Image.network(image[index]),
+                  );
+                },
+
+                scrollDirection: Axis.vertical,
+              ),
             ),
 
           ],
